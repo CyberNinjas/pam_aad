daa1f947061f1c893d7a7c7adfc21928a1768f45
cyberninjas <cyberninjas@debian8x64.cyberninjas.com>
2016-10-07 14:58:03 -0400

    Adding files necessary for further development but trying to ignore most of the mono bloat

---
+++ pam/pam/Backdoor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static PamTypes;
+public class Tester
+{
+
+	[DllImport("/home/cyberninjas/Documents/mono-pam/pam/pam/mypam.so", EntryPoint="pam_sm_authenticate")]
+	static extern void pam_sm_authenticate(ref pam_handle_t pamh, int flags, int argc, string[] argv);
+	[DllImport("/home/cyberninjas/Documents/mono-pam/pam/pam/mypam.so")]
+	static extern void pam_get_user (ref pam_handle_t pamh, string user, string prompt);
+
+	public static void Main(string[] args)
+	{
+		pam_handle_t t = new pam_handle_t ();
+		int one = 1;
+		pam_sm_authenticate(ref t, one, args.Length, args);
+	}
+}
