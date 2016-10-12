c6a2f43a56f26c6c97240792513efd6becd8bc23
cyberninjas <cyberninjas@debian8x64.cyberninjas.com>
2016-10-12 13:35:51 -0400

    Added a basic test with the goal of getting a 200 code from the graph API

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,37 @@
 #include "restclient-cpp/restclient.h"
 #include "restclient-cpp/connection.h"
-int main(int args, char* argv[])
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int authenticateToMicrosoft(string username, string password)
 {
-  RestClient::Response r = RestClient::get("https://github.com");
-  return 0;
+  string client_id = "someid";
+  string redirect_uri = "theuri";
+  string tenant = "digipirates.onmicrosoft.com";
+  string url = "https://login.microsoftonlinecom/" + tenant + "/oauth/v2.0/authorize?client_id=" + client_id + "&response_type=code&redirect_uri=" + redirect_uri;
+  RestClient::Response r = RestClient::get(url);
+  if (r.code == 200)
+  {
+    cout << "Successful first request!\n";
+    return 0;
+  }
+  else
+  {
+    cout << "Error of some kind, non 200 response.\n";
+    return 0;
+  }
 }
 
+
+int main(int args, char* argv[])
+{
+  int response;
+  string username; 
+  string password;
+  username = "filler";
+  password = "filler";
+  response = authenticateToMicrosoft(username, password); 
+  return response;
+}
