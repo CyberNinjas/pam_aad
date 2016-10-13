e9e47c68491b086423cf8557509d544539298952
cyberninjas <cyberninjas@debian8x64.cyberninjas.com>
2016-10-13 13:39:03 -0400

    Scaffolding:  - Note the skeleton function that will be used to poll the microsoft API for the token. It waits 5 seconds inbetween requests. We could make this extensible to take whatever suggested time period microsoft gives in    the message, but I haven't seen any other time than 5 seconds, so that's the default for the MVP  - Redacted the client-id. We'll have to figure out if we want the .so (when complete) to search out a specific text file on the system to come up with the client id/resource #.

--- src/main.cc
+++ src/main.cc
@@ -1,37 +1,76 @@
 #include "restclient-cpp/restclient.h"
 #include "restclient-cpp/connection.h"
+//#include "json.hpp"
 #include <string>
 #include <iostream>
-
+#include <unistd.h>
 using namespace std;
+//using json = nlohman::json;
 
-int authenticateToMicrosoft(string username, string password)
+string getClientRequestId()
 {
-  string client_id = "someid";
-  string redirect_uri = "theuri";
+  string client_request_id = "filler";
+  return client_request_id;
+}
+
+string  getDeviceCode()
+{
+  string client_id = "REDACTED";
+  string resource = "00000002-0000-0000-c000-000000000000";
   string tenant = "digipirates.onmicrosoft.com";
-  string url = "https://login.microsoftonlinecom/" + tenant + "/oauth/v2.0/authorize?client_id=" + client_id + "&response_type=code&redirect_uri=" + redirect_uri;
+  string client_request_id = getClientRequestId();
+  string url = "https://login.microsoftonline.com/" + tenant + "/oauth2/devicecode?resource=" + resource + "&client_id=" + client_id;
   RestClient::Response r = RestClient::get(url);
   if (r.code == 200)
   {
-    cout << "Successful first request!\n";
-    return 0;
+    cout << "\nSuccessful first request!\n";
+    cout << r.body + "\n";
+    return r.body;
   }
   else
   {
     cout << "Error of some kind, non 200 response.\n";
-    return 0;
+    cout <<"\nerror is: " + r.body + "\n";
+    cout <<"\nAnd the code is:";
+    cout << r.code;
+    return r.body;
   }
 }
 
+string getUriForPolling(string rawMessage){
+  string poll_uri = "filler";
+  return poll_uri;
+}
+
+string pollForToken(string poll_uri){
+  RestClient::Response r = RestClient::post(poll_uri, "text/json", "{\"foo\": \"bla\"}");
+  return r.body;
+}
+
+bool providedToken(string response_body){
+  return false;
+}
+
+int AuthenticateToMicrosoft(){
+  cout << "Welcome!\n";
+  string deviceCodeMessage = getDeviceCode();
+  string parsedUri = getUriForPolling(deviceCodeMessage);
+  cout << "\n" + deviceCodeMessage;
+  bool gotToken = false;
+  string response;
+  while (gotToken){
+    sleep(5000); //wait for 5 seconds before polling
+    response = pollForToken(parsedUri);
+    gotToken = providedToken(response);
+  } 
+
+}
 
 int main(int args, char* argv[])
 {
   int response;
   string username; 
   string password;
-  username = "filler";
-  password = "filler";
-  response = authenticateToMicrosoft(username, password); 
+  response = AuthenticateToMicrosoft(); 
   return response;
 }
