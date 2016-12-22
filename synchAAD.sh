66ab90ca4271209194d59fda891f617d21098458
SJCaldwell <scaldwell@cyberninjas.com>
2016-12-22 10:00:09 -0500

    SynchAAD is now doing all linking properly.

--- synchAAD.sh
+++ synchAAD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo g++ -g -Wall -shared -fPIC -o /lib/security/aad-pam.so src/connection.o src/helpers.o src/jwt.o src/utils.o /usr/lib/librestclient-cpp.a /usr/lib/libcryptopp.a /usr/lib/x86_64-linux-gnu/libpthread.a /usr/lib/x86_64-linux-gnu/libpam.a /usr/lib/x86_64-linux-gnu/libpam_misc.a /usr/lib/x86_64-linux-gnu/libcurl.a
+sudo g++ -g -Wall -shared -fPIC -o /lib/security/aad-pam.so src/connection.o src/helpers.o src/jwt.o src/utils.o /usr/lib/librestclient-cpp.a -lcryptopp /usr/lib/x86_64-linux-gnu/libpthread.a -lpam_misc -lpam -lssl -lcurl -lldap
