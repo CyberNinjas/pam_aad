c2787ad37a0137a0ec0d8356cd0088b334458784
SJCaldwell <scaldwell@cyberninjas.com>
2016-12-22 12:58:12 -0500

    At this stage, it compiles and can be run
    However, no output is being printed to the screen, making it largely useless.

--- synchAAD.sh
+++ synchAAD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo g++ -g -Wall -shared -fPIC -o /lib/security/aad-pam.so src/connection.o src/helpers.o src/jwt.o src/utils.o /usr/lib/librestclient-cpp.a -lcryptopp /usr/lib/x86_64-linux-gnu/libpthread.a -lpam_misc -lpam -lssl -lcurl -lldap
+sudo g++ -g -Wall -shared -fPIC -o /lib/security/aad-pam.so src/aad-pam.o src/jwt.o src/utils.o -lrestclient-cpp -lcryptopp /usr/lib/x86_64-linux-gnu/libpthread.a -lssl -lcurl -lldap
