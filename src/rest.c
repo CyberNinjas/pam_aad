577ac1e2ae47f19149b31a13b429503b030e11f4
SJCaldwell <scaldwell@cyberninjas.com>
2017-01-12 16:58:12 -0500

    Building out rest.c to dynamically generate post:
    - Currently resulting in a segmentation fault.
    - Have not found root issue yet, lost a lot of time
      on printf not printing if it's not at the end of a buffer.

--- src/rest.c
+++ src/rest.c
@@ -7,18 +7,27 @@
 #define PORT "443"
 
 int read_code_from_microsoft(const char *resource_id, const char *client_id, const char *tenant){
-    //initialize variables
+    /* initialize variables */
 
     BIO* bio;
-    SSL* ssl;
+    /* SSL* ssl; */
     SSL_CTX* ctx;
+    
+    char post_buf[1024];
 
-    // Registers the available SSL/TLS ciphers
-    // Starts security layer
+    /* Variables used to read the response from the server */
+    int size;
+    char buf[1024];
+
+    char write_buf[1024];
+
+
+    /* Registers the available SSL/TLS ciphers */
+    /* Starts security layer */
 
     SSL_library_init();
 
-    //creates a new SSL_CTX object as framework to establish TLS/SSL enabled connections
+    /* creates a new SSL_CTX object as framework to establish TLS/SSL enabled connections */
 
     ctx = SSL_CTX_new(SSLv23_client_method());
 
@@ -27,11 +36,11 @@ int read_code_from_microsoft(const char *resource_id, const char *client_id, con
         printf("Ctx is null\n");
     }
     
-    //Creates a new BIO chain consisting of an SSL BIO
+    /* Creates a new BIO chain consisting of an SSL BIO */
 
     bio = BIO_new_ssl_connect(ctx);
 
-    // uses the string name to set the hostname
+    /* uses the string name to set the hostname */
 
     BIO_set_conn_hostname(bio, HOST ":" PORT);
 
@@ -43,21 +52,35 @@ int read_code_from_microsoft(const char *resource_id, const char *client_id, con
     else{
         printf("Connected\n");
     }
+    printf("Directly before strcpy\n");
+    strcpy(post_buf, "text");
+    printf("strcpy ran fine\n");
+    strcat(post_buf, resource_id);
+    printf("first strcat\n");
+    //strcat(post_buf, "&client_id=");
+    //strcat(post_buf, client_id);
+    /* TODO: Change the below for a legitimate client request id, randomly generated */
+    //strcat(post_buf, "5929459294929");
 
-    // Data to create a HTTP request 
-    char * write_buf = "POST /digipirates.onmicrosoft.com/oauth2/devicecode/ HTTP/1.1\r\n"
-                        "Host: " HOST "\r\n"
-                        "Connection: close \r\n"
-                        "User-Agent: azure_authenticator_pam/1.0 \r\n"
-                        "Content-Length: 148\r\n"
-                        "\r\n"
-                        "?resource=00000002-0000-0000-c000-000000000000&client_id=34251358-1168-40c3-aced-100ee9b03090&client_request_id=7262ee1e-6f52-4855-867c-727fc64b26d5\r\n"
-                        "\r\n";
+    printf("post buf initialized"\n);
+    /* Data to create a HTTP request */
+    strcpy(write_buf, "POST /");
+    // strcat(write_buf, tenant);
+    // strcat(write_buf, "/oauth2/devicecode/ HTTP/1.1\r\n");
+    // strcat(write_buf, "Host: " HOST "\r\n");
+    // strcat(write_buf, "Connection: close \r\n");
+    // strcat(write_buf, "User-Agent: azure_authenticator_pam/1.0 \r\n");
+    // strcat(write_buf, "Content-Length: ");
+    // strcat(write_buf, (char*)sizeof(post_buf));
+    // strcat(write_buf, "\r\n");
+    // strcat(write_buf, "\r\n");
+    // strcat(write_buf, post_buf);
+    // strcat(write_buf, "\r\n");
 
-    //Attempts to write len bytes from buf to BIO
+    /* Attempts to write len bytes from buf to BIO */ 
     if (BIO_write(bio, write_buf, strlen(write_buf)) <= 0)
     {
-        //handle failed write here
+        /* handle failed write here */ 
         if (!BIO_should_retry(bio))
         {
             printf("Do retry\n");
@@ -66,16 +89,12 @@ int read_code_from_microsoft(const char *resource_id, const char *client_id, con
         printf("Failed write\n");
     }
 
-    //Variables used to read the response from the server
-    int size;
-    char buf[1024];
-
-    // Read the response
+    /* Read the response */
     for (;;)
     {
         size = BIO_read(bio, buf, 1023);
 
-        //If no more data, than exit the loop
+        /* If no more data, than exit the loop */
         if(size <= 0)
         {
             printf("\n\n");
@@ -90,18 +109,18 @@ int read_code_from_microsoft(const char *resource_id, const char *client_id, con
 
     return 0;
 }
-//purely for testing, takes no command line args
+/* purely for testing, takes no command line args */
 int main(){
-    //initialize variables
+    /* initialize variables */
     const char *resource_id;
     const char *client_id;
     const char *tenant;
 
-    //Provide hardcoded values for testing
+    /* Provide hardcoded values for testing */
     resource_id = "00000002-0000-0000-c000-000000000000";
     client_id = "7262ee1e-6f52-4855-867c-727fc64b26d5";
     tenant = "virtualvikings.onmicrosoft.com";
 
     read_code_from_microsoft(resource_id, client_id, tenant);
-
+    return 0;
 }
