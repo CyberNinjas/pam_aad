c5aa3ec0d8d8a5ad42699b1468266efe0dedb727
SJCaldwell <scaldwell@cyberninjas.com>
2017-01-17 15:26:31 -0500

    Updated signature for request_azure_signin_code().
    -No reason to pass the response_buffer.

--- src/rest.h
+++ src/rest.h
@@ -1,4 +1,4 @@
 
 int request_azure_signin_code(char *code, const char *resource_id, const char *client_id, const char *tenant);
 
-int request_azure_oauth_token(char *code, const char *resource_id, const char *client_id, char *response_buf, char *token_buf);
+int request_azure_oauth_token(char *code, const char *resource_id, const char *client_id, char *token_buf);
