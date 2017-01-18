69c2d5fa86f0338cac95edee6c3f0ec8c9b5be29
SJCaldwell <scaldwell@cyberninjas.com>
2017-01-18 11:06:06 -0500

    Updated rest.c to accomadate device_code.
    - There are two codes associated with the initial device call.
    - I was sending off the user_code as the device_code, which
      was resulting in a rejected code.
    - This mostly fixes that problem. However CURRENTLY: the token
      buffer does not have the access token in it. It has some small nonsense value.
      Have to figure that out.

--- src/rest.h
+++ src/rest.h
@@ -1,4 +1,4 @@
 
-int request_azure_signin_code(char *code, const char *resource_id, const char *client_id, const char *tenant);
+int request_azure_signin_code(char *code, const char *resource_id, const char *client_id, const char *tenant, char *device_code);
 
 int request_azure_oauth_token(char *code, const char *resource_id, const char *client_id, char *token_buf);
