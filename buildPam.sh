52cbcb42f3c8b17f3778588e4b02f90275c73b15
SJCaldwell <scaldwell@cyberninjas.com>
2017-01-20 15:17:05 -0500

    Buildpam is updated with the jwtlib dependency.
    - jwt.c now has two major methods for creating a jwt strucure and
      validating it.
    - jwt.h opens these methods up to other files.

--- buildPam.sh
+++ buildPam.sh
@@ -2,6 +2,6 @@
 
 gcc -fPIC -fno-stack-protector -c src/aad.c src/rest.c src/cJSON.c
 
-sudo ld -x --shared -o /lib/security/aad.so -lssl -lcrypto -lm aad.o rest.o cJSON.o
+sudo ld -x --shared -o /lib/security/aad.so -lssl -lcrypto -lm -ljwt aad.o rest.o cJSON.o
 
 rm aad.o
