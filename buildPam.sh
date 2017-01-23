8bdcc8fcf7541d1028e6a921d95475ce81aced24
SJCaldwell <scaldwell@cyberninjas.com>
2017-01-23 10:29:00 -0500

    Updated build script to handle new dependencies.
    -Jwt library added.
    -utils.c added.

--- buildPam.sh
+++ buildPam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -fPIC -fno-stack-protector -c src/aad.c src/rest.c src/cJSON.c
+gcc -fPIC -fno-stack-protector -c src/aad.c src/rest.c src/cJSON.c src/utils.h src/jwt.c
 
-sudo ld -x --shared -o /lib/security/aad.so -lssl -lcrypto -lm -ljwt aad.o rest.o cJSON.o
+sudo ld -x --shared -o /lib/security/aad.so -lssl -lcrypto -lm -ljwt aad.o rest.o cJSON.o utils.o jwt.c
 
 rm aad.o
