fd8006b63b07e40e77ce2938b899c6152fa4f58a
Lucas Ramage <lramage@cyberninjas.com>
2018-12-05 19:14:35 +0000

    Update GNU Build System
    Bugs: https://github.com/CyberNinjas/pam_aad/issues/13

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -x
+
+umask 022
+autoreconf -fiv
--- buildPam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc -fPIC -fno-stack-protector -c src/aad.c src/rest.c src/cJSON.c src/utils.c src/jwt.c -w
-
-sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/aad.so -lssl -lcrypto -lm -ljwt aad.o rest.o cJSON.o utils.o jwt.o
-
-rm aad.o
