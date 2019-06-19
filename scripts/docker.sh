59a0c61eab9d81b3747151b92b06f1f5219f81b7
Lucas Ramage <lramage@cyberninjas.com>
2019-06-19 20:18:41 +0000

    Auto-deploy on tagged releases

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,7 +5,8 @@ set -eu
 recursive_build() {
   path=$1
   tag=$2
-  extra=$3
+  release=$3
+  extra=$4
   for dir in $path/docker/*; do
     if [ -d "${dir}" ]; then
       distro=$(basename "${dir}")
@@ -13,7 +14,9 @@ recursive_build() {
       if [ ! -z "${extra}" ]; then
         image="${tag}:${distro}-${extra}" # org/image:tag-extra
       fi
-      docker build -t "${image}" "${path}" \
+      docker build --build-arg VERSION="${release}" \
+                   --build-arg DEBVER=1 \
+                   -t "${image}" "${path}" \
                    -f "${dir}/Dockerfile"
     fi
   done
@@ -21,12 +24,15 @@ recursive_build() {
 
 main() {
   DEFAULT_IMAGE="cyberninjas/pam_aad"
+  export RELEASE=$(git describe --tags $(git rev-list --tags --max-count=1))
 
   # Build all docker images
-  recursive_build . "${DEFAULT_IMAGE}" ''
+  recursive_build . "${DEFAULT_IMAGE}" "${RELEASE}" ''
 
   # Build all testing docker images
-  recursive_build ./test "${DEFAULT_IMAGE}" 'testing'
+  recursive_build ./test "${DEFAULT_IMAGE}" "${RELEASE}" 'testing'
+
+  sed "s/{{version}}/${VERSION}/" .bintray.json.in > bintray.json
 }
 
 main
