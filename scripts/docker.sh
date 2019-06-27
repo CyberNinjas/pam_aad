21c77a02ca1720b61c2c7e810f45ed87a5a26176
Lucas Ramage <lramage@cyberninjas.com>
2019-06-27 19:55:39 +0000

    Fix VERSION: parameter not set
    Bug: https://github.com/CyberNinjas/pam_aad/issues/44

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,20 +11,31 @@ recursive_build() {
     if [ -d "${dir}" ]; then
       distro=$(basename "${dir}")
       image="${tag}:${distro}" # org/image:tag
+
       if [ ! -z "${extra}" ]; then
         image="${tag}:${distro}-${extra}" # org/image:tag-extra
       fi
-      docker build --build-arg VERSION="${release}" \
-                   --build-arg DEBVER=1 \
-                   -t "${image}" "${path}" \
-                   -f "${dir}/Dockerfile"
+
+      case "${distro}" in
+        debian|ubuntu)
+          docker build --build-arg VERSION="${release}" \
+                       --build-arg DEBVER=1 \
+                       -t "${image}" "${path}" \
+                       -f "${dir}/Dockerfile"
+          ;;
+        *)
+          docker build -t "${image}" "${path}" \
+                       -f "${dir}/Dockerfile"
+          ;;
+      esac
     fi
   done
 }
 
 main() {
   DEFAULT_IMAGE="cyberninjas/pam_aad"
-  export RELEASE=$(git describe --tags $(git rev-list --tags --max-count=1))
+  RELEASE=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+  export RELEASE
 
   # Build all docker images
   recursive_build . "${DEFAULT_IMAGE}" "${RELEASE}" ''
@@ -32,7 +43,7 @@ main() {
   # Build all testing docker images
   recursive_build ./test "${DEFAULT_IMAGE}" "${RELEASE}" 'testing'
 
-  sed "s/{{version}}/${VERSION}/" .bintray.json.in > bintray.json
+  sed "s/{{version}}/${RELEASE}/" .bintray.json.in > bintray.json
 }
 
 main
